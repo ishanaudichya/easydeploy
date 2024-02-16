526314d745d4fbba734bcdd1b47979ebf9dbd640
ishanaudichya <93155612+ishanaudichya@users.noreply.github.com>
2024-02-16 22:06:09 +0530

    Update script.js

--- build_server/script.js
+++ build_server/script.js
@@ -1,74 +1,73 @@
-const { exec } = require('child_process')
-const path = require('path')
-const fs = require('fs')
-const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3')
-const mime = require('mime-types')
-const Redis = require('ioredis')
+const { exec } = require("child_process");
+const path = require("path");
+const fs = require("fs");
+const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
+const mime = require("mime-types");
+// const Redis = require("ioredis");
+// const publisher = new Redis(redisuri);
 
-
-const publisher = new Redis('')
-
-
+// const redisuri = process.env.REDIS_URL;
 const s3Client = new S3Client({
-    region: '',
-    credentials: {
-        accessKeyId: '',
-        secretAccessKey: ''
-    }
-})
+  region: "ap-south-1",
+  credentials: {
+    accessKeyId: process.env.ACCESS_KEY_ID,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY,
+  },
+});
 
-const PROJECT_ID = process.env.PROJECT_ID
+const PROJECT_ID = process.env.PROJECT_ID;
 
-
-function publishLog(log) {
-    publisher.publish(`logs:${PROJECT_ID}`, JSON.stringify({ log }))
-}
+// function publishLog(log) {
+//   publisher.publish(`logs:${PROJECT_ID}`, JSON.stringify({ log }));
+// }
 
 async function init() {
-    console.log('Executing script.js')
-    publishLog('Build Started...')
-    const outDirPath = path.join(__dirname, 'output')
+  console.log("Executing script.js");
+  // publishLog("Build Started...");
+  const outDirPath = path.join(__dirname, "output");
 
-    const p = exec(`cd ${outDirPath} && npm install && npm run build`)
+  const p = exec(`cd ${outDirPath} && npm install && npm run build`);
 
-    p.stdout.on('data', function (data) {
-        console.log(data.toString())
-        publishLog(data.toString())
-    })
+  p.stdout.on("data", function (data) {
+    console.log(data.toString());
+    // publishLog(data.toString());
+  });
 
-    p.stdout.on('error', function (data) {
-        console.log('Error', data.toString())
-        publishLog(`error: ${data.toString()}`)
-    })
+  p.stdout.on("error", function (data) {
+    console.log("Error", data.toString());
+    // publishLog(`error: ${data.toString()}`);
+  });
 
-    p.on('close', async function () {
-        console.log('Build Complete')
-        publishLog(`Build Complete`)
-        const distFolderPath = path.join(__dirname, 'output', 'dist')
-        const distFolderContents = fs.readdirSync(distFolderPath, { recursive: true })
+  p.on("close", async function () {
+    console.log("Build Complete");
+    // publishLog(`Build Complete`);
+    const distFolderPath = path.join(__dirname, "output", "dist");
+    const distFolderContents = fs.readdirSync(distFolderPath, {
+      recursive: true,
+    });
 
-        publishLog(`Starting to upload`)
-        for (const file of distFolderContents) {
-            const filePath = path.join(distFolderPath, file)
-            if (fs.lstatSync(filePath).isDirectory()) continue;
+    // publishLog(`Starting to upload`);
+    for (const file of distFolderContents) {
+      const filePath = path.join(distFolderPath, file);
+      if (fs.lstatSync(filePath).isDirectory()) continue;
 
-            console.log('uploading', filePath)
-            publishLog(`uploading ${file}`)
+      console.log("uploading", filePath);
+      // publishLog(`uploading ${file}`);
 
-            const command = new PutObjectCommand({
-                Bucket: 'vercel-clone-outputs',
-                Key: `__outputs/${PROJECT_ID}/${file}`,
-                Body: fs.createReadStream(filePath),
-                ContentType: mime.lookup(filePath)
-            })
+      const command = new PutObjectCommand({
+        Bucket: "vercel-clone-outputs",
+        Key: `__outputs/${PROJECT_ID}/${file}`,
+        Body: fs.createReadStream(filePath),
+        ContentType: mime.lookup(filePath),
+      });
 
-            await s3Client.send(command)
-            publishLog(`uploaded ${file}`)
-            console.log('uploaded', filePath)
-        }
-        publishLog(`Done`)
-        console.log('Done...')
-    })
+      await s3Client.send(command);
+      // publishLog(`uploaded ${file}`);
+      console.log("uploaded", filePath);
+    }
+    // publishLog(`Done`);
+    console.log("Done...");
+  });
 }
 
-init()
+init();
