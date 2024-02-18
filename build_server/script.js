9a70e86f0963258e96e20f6ddd214809f5085708
ishanaudichya <93155612+ishanaudichya@users.noreply.github.com>
2024-02-19 03:54:07 +0530

    implement redis log pushing

--- build_server/script.js
+++ build_server/script.js
@@ -3,10 +3,11 @@ const path = require("path");
 const fs = require("fs");
 const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
 const mime = require("mime-types");
-// const Redis = require("ioredis");
-// const publisher = new Redis(redisuri);
+const Redis = require("ioredis");
 
-// const redisuri = process.env.REDIS_URL;
+const redisuri = process.env.REDIS_URL;
+const publisher = new Redis();
+
 const s3Client = new S3Client({
   region: "ap-south-1",
   credentials: {
@@ -17,42 +18,42 @@ const s3Client = new S3Client({
 
 const PROJECT_ID = process.env.PROJECT_ID;
 
-// function publishLog(log) {
-//   publisher.publish(`logs:${PROJECT_ID}`, JSON.stringify({ log }));
-// }
+function publishLog(log) {
+  publisher.publish(`logs:${PROJECT_ID}`, JSON.stringify({ log }));
+}
 
 async function init() {
   console.log("Executing script.js");
-  // publishLog("Build Started...");
+  publishLog("Build Started...");
   const outDirPath = path.join(__dirname, "output");
 
   const p = exec(`cd ${outDirPath} && npm install && npm run build`);
 
   p.stdout.on("data", function (data) {
     console.log(data.toString());
-    // publishLog(data.toString());
+    publishLog(data.toString());
   });
 
   p.stdout.on("error", function (data) {
     console.log("Error", data.toString());
-    // publishLog(`error: ${data.toString()}`);
+    publishLog(`error: ${data.toString()}`);
   });
 
   p.on("close", async function () {
     console.log("Build Complete");
-    // publishLog(`Build Complete`);
+    publishLog(`Build Complete`);
     const distFolderPath = path.join(__dirname, "output", "dist");
     const distFolderContents = fs.readdirSync(distFolderPath, {
       recursive: true,
     });
 
-    // publishLog(`Starting to upload`);
+    publishLog(`Starting to upload`);
     for (const file of distFolderContents) {
       const filePath = path.join(distFolderPath, file);
       if (fs.lstatSync(filePath).isDirectory()) continue;
 
       console.log("uploading", filePath);
-      // publishLog(`uploading ${file}`);
+      publishLog(`uploading ${file}`);
 
       const command = new PutObjectCommand({
         Bucket: "netlify-clone",
@@ -62,10 +63,10 @@ async function init() {
       });
 
       await s3Client.send(command);
-      // publishLog(`uploaded ${file}`);
+      publishLog(`uploaded ${file}`);
       console.log("uploaded", filePath);
     }
-    // publishLog(`Done`);
+    publishLog(`Done`);
     console.log("Done...");
   });
 }
