2cd1d6f2d9414a0511ec5441986c6a56dde6f4f1
ishanaudichya <93155612+ishanaudichya@users.noreply.github.com>
2024-02-18 13:14:38 +0530

    proxy init

---
+++ proxy_service/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const httpProxy = require("http-proxy");
+
+const app = express();
+const PORT = 8000;
+
+const BASE_PATH = "https://netlify-clone.s3.ap-south-1.amazonaws.com/__outputs";
+
+const proxy = httpProxy.createProxy();
+
+app.use((req, res) => {
+  const host = req.hostname;
+  const subdomain = host.split(".")[0];
+  const resolvesTo = `${BASE_PATH}/${subdomain}`;
+
+  return proxy.web(req, res, { target: resolvesTo, changeOrigin: true });
+});
+
+proxy.on("proxyReq", (proxyReq, req, res) => {
+  const url = req.url;
+  if (url === "/") proxyReq.path += "index.html";
+});
+
+app.listen(PORT, () => console.log(`Proxy Running at${PORT}`));
