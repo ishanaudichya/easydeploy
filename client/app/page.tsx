c932f6316c1defb285e62903ff2cb14e8ff9265d
ishanaudichya <93155612+ishanaudichya@users.noreply.github.com>
2024-02-21 04:16:58 +0530

    fix/log show | confetti

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -6,6 +6,8 @@ import { Input } from "@/components/ui/input";
 import { Github } from "lucide-react";
 import { Fira_Code } from "next/font/google";
 import axios from "axios";
+import Confetti from 'react-confetti';
+
 
 const socket = io("http://localhost:9002");
 
@@ -13,7 +15,7 @@ const firaCode = Fira_Code({ subsets: ["latin"] });
 
 export default function Home() {
   const [repoURL, setURL] = useState<string>("");
-
+  const [showUrl, setShowUrl] = useState(false);
   const [logs, setLogs] = useState<string[]>([]);
 
   const [loading, setLoading] = useState(false);
@@ -53,9 +55,19 @@ export default function Home() {
 
   const handleSocketIncommingMessage = useCallback((message: string) => {
     console.log(`[Incomming Socket Message]:`, typeof message, message);
+    if(message[0]==='J'){
+      setLogs((prev) => [...prev, "Deploying..."]);
+    }
+    else if(message==='{"log":"Done"}'){
+      setShowUrl(true);
+      setLogs((prev) => [...prev, "Done"]);
 
-    // const { log } = JSON.parse(message);
-    setLogs((prev) => [...prev, message]);
+    }
+    else{
+      
+      const { log } = JSON.parse(message);
+      setLogs((prev) => [...prev, log]);
+    }
     
     logContainerRef.current?.scrollIntoView({ behavior: "smooth" });
   }, []);
@@ -70,6 +82,11 @@ export default function Home() {
 
   return (
     <>
+    {showUrl && 
+      <Confetti   
+      />
+      
+      }
       <nav className="text-white p-4">
         <div className="container mx-auto flex justify-between items-center">
           <a href="/" className="text-2xl font-bold"><span className="bg-gradient-to-r from-purple-500 via-indigo-400 to-purple-300 inline-block text-transparent bg-clip-text"> Easy</span>Deploy</a>
@@ -95,23 +112,11 @@ export default function Home() {
             onClick={handleClickDeploy}
             disabled={!isValidURL[0] || loading}
             className="w-full mt-3"
-          >
-            {loading ? "In Progress" : "Deploy"}
+          >{showUrl ?  "Deployed" : (loading ? "In Progress" : "Deploy")}
+            
+            
           </Button>
-          {deployPreviewURL && (
-            <div className="mt-2 bg-slate-900 py-4 px-2 rounded-lg">
-              <p>
-                Preview URL{" "}
-                <a
-                  target="_blank"
-                  className="text-sky-400 bg-sky-950 px-3 py-2 rounded-lg"
-                  href={deployPreviewURL}
-                >
-                  {deployPreviewURL}
-                </a>
-              </p>
-            </div>
-          )}
+          
           {logs.length > 0 && (
             <div
               className={`${firaCode.className} text-sm text-green-500 logs-container mt-5 border-green-500 border-2 rounded-lg p-4 h-[300px] overflow-y-auto`}
@@ -124,6 +129,20 @@ export default function Home() {
                   >{`> ${log}`}</code>
                 ))}
               </pre>
+            </div>
+          )}
+          {showUrl && (
+            <div className="mt-2 bg-slate-900 py-4 px-2 rounded-lg">
+              <p>
+                Preview URL{" "}
+                <a
+                  target="_blank"
+                  className="text-sky-400 bg-sky-950 px-3 py-2 rounded-lg"
+                  href={deployPreviewURL}
+                >
+                  {deployPreviewURL}
+                </a>
+              </p>
             </div>
           )}
         </div>
