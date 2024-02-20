a4a83daa5941ec1dfaf61219c2193a809301acce
ishanaudichya <93155612+ishanaudichya@users.noreply.github.com>
2024-02-20 23:40:16 +0530

    client update

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -72,17 +72,17 @@ export default function Home() {
     <>
       <nav className="text-white p-4">
         <div className="container mx-auto flex justify-between items-center">
-          <a href="/" className="text-lg font-bold">Netlify Clone</a>
+          <a href="/" className="text-2xl font-bold"><span className="bg-gradient-to-r from-purple-500 via-indigo-400 to-purple-300 inline-block text-transparent bg-clip-text"> Easy</span>Deploy</a>
           <div className="flex items-center gap-4">
-            <a href="/about" className="hover:bg-slate-900 hover:text-white px-3 py-2 rounded transition duration-150 ease-in-out">About</a>
-            <a href="/contact" className="hover:bg-slate-900 hover:text-white px-3 py-2 rounded transition duration-150 ease-in-out">Contact</a>
+            <a href="https://github.com/ishanaudichya/" className="hover:bg-slate-900 hover:text-white px-3 py-2 rounded transition duration-150 ease-in-out">About</a>
+            <a href="https://ishanaudichya.netlify.app" className="hover:bg-slate-900 hover:text-white px-3 py-2 rounded transition duration-150 ease-in-out">Contact</a>
           </div>
         </div>
       </nav>
       <main className="flex justify-center items-center h-[calc(100vh-175px)]">
         <div className="w-[600px]">
           <span className="flex justify-start items-center gap-2">
-            {/* <Github className="text-5xl" /> */}
+            <Github className="text-5xl" />
             <Input
               disabled={loading}
               value={repoURL}
@@ -128,10 +128,16 @@ export default function Home() {
           )}
         </div>
       </main>
-      <footer className="text-slate-500 py-4 mt-8 ">
-      <div className="container mx-auto text-right ">
+      <footer className="text-slate-500 py-4 mt-8 text-xs">
+      <div className="test-xs mx-auto container flex items-center justify-center">
+        <div className="flex-1"></div>
+        <div className="flex-1 text-center">
+          Developed by <a href="https://ishanaudichya.netlify.app"><u>Ishan Audichya </u></a>
+            
+          </div>
+        <div className="flex-1 text-right">
           <a
-            href="https://github.com/yourGithubUsername"
+            href="https://github.com/ishanaudichya"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block hover:underline px-4 text-xs"
@@ -139,13 +145,14 @@ export default function Home() {
             GitHub
           </a>
           <a
-            href="https://www.linkedin.com/in/yourLinkedInProfile"
+            href="https://www.linkedin.com/in/ishan-audichya/"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block hover:underline text-xs"
           >
             LinkedIn
           </a>
+          </div>
         </div>
       </footer>
     </>
