fb9daf19aeeb6e26e1eb0dec984dcc896873dcb0
Vikash-Sarraf <vsarraf15@gmail.com>
2024-02-20 21:08:17 +0530

    Adding navbar and footer

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -69,54 +69,85 @@ export default function Home() {
   }, [handleSocketIncommingMessage]);
 
   return (
-    <main className="flex justify-center items-center h-[100vh]">
-      <div className="w-[600px]">
-        <span className="flex justify-start items-center gap-2">
-          {/* <Github className="text-5xl" /> */}
-          <Input
-            disabled={loading}
-            value={repoURL}
-            onChange={(e) => setURL(e.target.value)}
-            type="url"
-            placeholder="Github URL"
-          />
-        </span>
-        <Button
-          onClick={handleClickDeploy}
-          disabled={!isValidURL[0] || loading}
-          className="w-full mt-3"
-        >
-          {loading ? "In Progress" : "Deploy"}
-        </Button>
-        {deployPreviewURL && (
-          <div className="mt-2 bg-slate-900 py-4 px-2 rounded-lg">
-            <p>
-              Preview URL{" "}
-              <a
-                target="_blank"
-                className="text-sky-400 bg-sky-950 px-3 py-2 rounded-lg"
-                href={deployPreviewURL}
-              >
-                {deployPreviewURL}
-              </a>
-            </p>
+    <>
+      <nav className="text-white p-4">
+        <div className="container mx-auto flex justify-between items-center">
+          <a href="/" className="text-lg font-bold">Netlify Clone</a>
+          <div className="flex items-center gap-4">
+            <a href="/about" className="hover:bg-slate-900 hover:text-white px-3 py-2 rounded transition duration-150 ease-in-out">About</a>
+            <a href="/contact" className="hover:bg-slate-900 hover:text-white px-3 py-2 rounded transition duration-150 ease-in-out">Contact</a>
           </div>
-        )}
-        {logs.length > 0 && (
-          <div
-            className={`${firaCode.className} text-sm text-green-500 logs-container mt-5 border-green-500 border-2 rounded-lg p-4 h-[300px] overflow-y-auto`}
+        </div>
+      </nav>
+      <main className="flex justify-center items-center h-[calc(100vh-175px)]">
+        <div className="w-[600px]">
+          <span className="flex justify-start items-center gap-2">
+            {/* <Github className="text-5xl" /> */}
+            <Input
+              disabled={loading}
+              value={repoURL}
+              onChange={(e) => setURL(e.target.value)}
+              type="url"
+              placeholder="Github URL"
+            />
+          </span>
+          <Button
+            onClick={handleClickDeploy}
+            disabled={!isValidURL[0] || loading}
+            className="w-full mt-3"
           >
-            <pre className="flex flex-col gap-1">
-              {logs.map((log, i) => (
-                <code
-                  ref={logs.length - 1 === i ? logContainerRef : undefined}
-                  key={i}
-                >{`> ${log}`}</code>
-              ))}
-            </pre>
-          </div>
-        )}
-      </div>
-    </main>
+            {loading ? "In Progress" : "Deploy"}
+          </Button>
+          {deployPreviewURL && (
+            <div className="mt-2 bg-slate-900 py-4 px-2 rounded-lg">
+              <p>
+                Preview URL{" "}
+                <a
+                  target="_blank"
+                  className="text-sky-400 bg-sky-950 px-3 py-2 rounded-lg"
+                  href={deployPreviewURL}
+                >
+                  {deployPreviewURL}
+                </a>
+              </p>
+            </div>
+          )}
+          {logs.length > 0 && (
+            <div
+              className={`${firaCode.className} text-sm text-green-500 logs-container mt-5 border-green-500 border-2 rounded-lg p-4 h-[300px] overflow-y-auto`}
+            >
+              <pre className="flex flex-col gap-1">
+                {logs.map((log, i) => (
+                  <code
+                    ref={logs.length - 1 === i ? logContainerRef : undefined}
+                    key={i}
+                  >{`> ${log}`}</code>
+                ))}
+              </pre>
+            </div>
+          )}
+        </div>
+      </main>
+      <footer className="text-slate-500 py-4 mt-8 ">
+      <div className="container mx-auto text-right ">
+          <a
+            href="https://github.com/yourGithubUsername"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="inline-block hover:underline px-4 text-xs"
+          >
+            GitHub
+          </a>
+          <a
+            href="https://www.linkedin.com/in/yourLinkedInProfile"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="inline-block hover:underline text-xs"
+          >
+            LinkedIn
+          </a>
+        </div>
+      </footer>
+    </>
   );
 }
